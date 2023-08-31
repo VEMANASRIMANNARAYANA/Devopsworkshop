6d070e9f19300d8d5a84137048dfc60b9593608a
VEMANASRIMANNARAYANA <142872469+VEMANASRIMANNARAYANA@users.noreply.github.com>
2023-08-31 15:19:34 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "SRIMANNARAYANA" );
+        System.out.println( "THIS IS WORKSHOP SESSION JENKINS" );
     }
 }
